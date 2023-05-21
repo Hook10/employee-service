8d9ae4a1f490a6e29972c1e21bdbd3d19fd88ca9
AskarSap <sapasask@gmail.com>
2023-05-21 18:32:30 +0600

    feature_config-server

--- src/main/java/com/hook/emp/controller/EmployeeController.java
+++ src/main/java/com/hook/emp/controller/EmployeeController.java
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("api/employees")
+@RequestMapping("/api/employees")
 @AllArgsConstructor
 public class EmployeeController {
 
